5ed8fdb2c486b1090f5a975a699013772329d025
System Administrator <root@Herberts-MacBook-Pro.local>
2014-03-12 11:26:28 -0500

    add rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reservester::Application.config.secret_key_base = '5de73bf9a55e8a533e1b16a025b6c5c7f347b9e8a13f1b3263c7270764d17f458eed0345a84c0b51d0fac77deb2ff7078e1bdb4af3f59f39c5d5b388c49b333b'
